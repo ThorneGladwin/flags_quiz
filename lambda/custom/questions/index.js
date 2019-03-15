50ed8a66741d3657a69b9c545b042174b6e862df
Thorne Gladwin <thorne.gladwin@eurostar.com>
2019-03-15 11:19:40 +0000

    Added a small set of questions

---
+++ lambda/custom/questions/index.js
@@ -0,0 +1,17 @@
+questions = [
+    { answer: "Afghanistan", question: "Three equal vertical bands of black, red and green. There is a mosque with flags either side in the center of the flag." },
+    { answer: "Albania", question: "Red with a black two-headed eagle in the center." },
+    { answer: "Algeria", question: "Two equal vertical bands of green and white. There is a red crescent with a five pointed start in the center of the flag." },
+    { answer: "Andorra", question: "Three vertical bands of blue, yellow and red, with the national coat of arms in the center. The coat of arms features a quartered shield with the motto 'Virtus Unita Fortior' underneath." },
+    { answer: "Angola", question: "Two equal horizontal bands of red and black, with a centered yellow emblem consisting of a five-pointed star within half a cogwheel crossed by a machete." },
+    { answer: "Argentina", question: "Three horizontal bands of sky blue, white and sky blue. In the center is a radiant yellow sun with a human face known as the sun of may." },
+    { answer: "Armenia", question: "Three equal horizontal bands of red, blue and orange." },
+    { answer: "Australia", question: "Blue with the union jack in the top left. There are 5 stars to the right of the union jack and 1 star underneath it." },
+    { answer: "Austria", question: "Three equal horizontal bands of red, white and red." },
+    { answer: "Azerbaijan", question: "Three equal horizontal bands of sky blue, red and green. There is a white vertical crescent moon and an eight-pointed star in the center." },
+    { answer: "Bangladesh", question: "Green with a large red circle in the center." }
+];
+
+module.exports = {
+    questions
+};
